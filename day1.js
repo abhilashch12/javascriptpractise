9c1ebea0b365a3c6c84cb2417cada6162fe02000
abhilahch12 <abhilashreddych12@gmail.com>
2026-04-28 15:03:59 +0530

    day1

---
+++ day1.js
@@ -0,0 +1,8 @@
+const Name = "abhilash";
+const Age = 23;
+const isStudent = true;
+let score=50;
+const city ='hyderabad';
+console.log(`hi,i am ${Name} and i live in ${city}`);
+Age=22;
+console.log(typeof(city));
