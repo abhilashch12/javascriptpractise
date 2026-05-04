e91ac5b9ec2bd129c7c5eba3266f25f68bd97420
abhilahch12 <abhilashreddych12@gmail.com>
2026-05-04 15:27:45 +0530

    promise.all

--- phase2/day1-2.js
+++ phase2/day1-2.js
@@ -8,11 +8,37 @@ function fakeFetch(data,delay){
 }
 async function run(){
     try{
-    let p = await fakeFetch("abhilash",2000);
+    let start = Date.now();
+    console.log(start);
+    let p = await Promise.all([fakeFetch("abhilash",1000),fakeFetch("abhilash",2000),fakeFetch("abhilash",3000)]);
     console.log(p);
+    let end = Date.now();
+    console.log(end);
+    console.log("time taken",end-start);
     }
-    catch{
+    catch(error){
      console.log(error);
     }
 }
 run();
+function fakeFailFetch(score){
+    let p = new Promise((resolve,reject)=>{
+        if(score>30){
+            resolve(`pass`);
+        }
+        else{
+            reject("fail")
+        }
+    });
+    return p;
+}
+async function display(){
+    try{
+        let p = await fakeFailFetch(25);
+        console.log(p);
+    }
+    catch(error){
+        console.log(error);
+    }
+};
+display();
